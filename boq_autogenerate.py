81cd0b7b93f7c05f1161cfb1536caf294f594274
Bhavana <bhavananetcon@gmail.com>
2025-08-04 15:57:36 +0530

    Update boq_autogenerate.py

--- boq_autogenerate.py
+++ boq_autogenerate.py
@@ -1,4 +1,12 @@
 import pandas as pd
 def generate_boq(input_file, output_file):
   try:
+    df=pd.read_excel(input_file)
+    boq=df.groupby(['category' , 'product'])['quantity'].sum().reset_index()
+    boq.to_excel(output_file, index=False)
+    print(f"Boq generated successfully and saved as {output_file}")
+  except Exception as e:
+    print("error:" , e)
+
+generate_boq('input_template.xlsx", 'sample_output.xlsx')
       
